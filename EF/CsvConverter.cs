635e9e6d345fe30ebff7e693514be2f7e0d22826
FloOoH <florian.gerstner@arcor.de>
2022-12-22 16:36:10 +0100

    changes for real csvs

---
+++ EF/CsvConverter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace EF
+{
+    internal class CsvConverter
+    {
+        private string paths;
+
+        public CsvConverter(string paths)
+        {
+            this.paths = paths;
+
+        }
+        internal IEnumerable<string[]>[] readCsvToStringList(string paths)
+        {
+            try
+            {
+                string str = Environment.CurrentDirectory;
+                string csvtest = str + @"\dependencies\LGT_2019-08-17.csv";
+                List<string> allpaths = new List<string>();
+                allpaths.Add(csvtest);
+                allpaths.Add(csvtest);
+                var allContents = new IEnumerable<string[]>[allpaths.Count()];
+                foreach (var (path, index) in allpaths.Select((value, i) => (value, i)))
+                {
+                    var rarecontents = File.ReadAllText(path).Split('\n');
+                    var csvdata = from line in rarecontents
+                                  select line.Split(',').ToArray();
+                    int headerrowscsv = allpaths.Count();
+                    foreach (var rowcsv in csvdata.Skip(headerrowscsv)
+                        .TakeWhile(r => r.Length > 1 && r.Last().Trim().Length > 0))
+                    {
+                        var parentName = rowcsv[0];
+                        var quantity = rowcsv[1];
+                        var componentName = rowcsv[2];
+                    }
+                    allContents[index] = csvdata;
+                }
+                return allContents;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.ToString());
+            }
+        }
+    }
+}
--- EF/Program.cs
+++ EF/Program.cs
@@ -4,7 +4,9 @@ using System.Linq;
 namespace EF
 {
     internal class Program
-    { 
+    {
+        public static string paths { get; private set; }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Database CSV Converter Importer V_0.1");
@@ -12,7 +14,8 @@ namespace EF
             TableInsert mytablereading = new TableInsert();
             var loginids = mytablereading.ReadingIndex(AbstractImport.Table.Logins);
             var loginid = loginids.Count()+1;
-
+            CsvConverter myconvert = new CsvConverter(paths);
+            var mycsvdata = myconvert.readCsvToStringList(paths);
             for (int i = 0; i < 3; i++)
             {
                 TableInsert mytableinsert = new TableInsert();
